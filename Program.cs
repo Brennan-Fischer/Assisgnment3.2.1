4c21b048e181efee537a73acc8ed6b0f863e7a2b
Brennan <blfische92@gmail.com>
2024-05-20 21:49:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assisgnment3._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[2,4]
+            { {1 , 2, 3, 4 },
+            {  5, 6, 7, 8}
+            }; 
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(myArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
